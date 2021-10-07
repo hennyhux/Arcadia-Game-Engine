517ce4f3240cd83c02606e183d7b8b33a498224c
Beto Cruz <cruzraygada.2@osu.edu>
2021-10-07 18:15:44 -0400

    Added TileMapDefinition Folder

---
+++ TileMapDefinition/LevelDefinition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameSpace.Enums;
+
+namespace GameSpace.TileMapDefinition
+{
+    class LevelDefinition
+    {
+        public class Obstacles
+        {
+            public BlockID block;
+            public int x;
+            public int y;
+        }
+        public class Enemies
+        {
+            public EnemyID enemy;
+            public int x;
+            public int y;
+        }
+        public class Items
+        {
+            public ItemID item;
+            public int x;
+            public int y;
+        }
+    }
+}
