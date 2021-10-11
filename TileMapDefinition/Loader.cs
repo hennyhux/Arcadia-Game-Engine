960b3adb20a24927a0f8fdac4f5cda7b8fde15d2
Beto Cruz <cruzraygada.2@osu.edu>
2021-10-10 23:34:49 -0400

    Added more stuff to the xml file. Handled starting facing position.

--- TileMapDefinition/Loader.cs
+++ TileMapDefinition/Loader.cs
@@ -144,14 +144,15 @@ namespace GameSpace.TileMapDefinition
             foreach (Avatar avatarObject in avatarList)
             {
                 Vector2 location = new Vector2(avatarObject.x, avatarObject.y);
-                //avatarsList.Add(marioFactory.ReturnMario(location));
                 switch (avatarObject.facing)
                 {
                     case eFacing.LEFT:
                         avatarsList.Add(marioFactory.ReturnMario(location));
                         break;
                     case eFacing.RIGHT:
-                        avatarsList.Add(marioFactory.ReturnMario(location));
+                        avatar = marioFactory.ReturnMario(location);
+                        avatar.FaceRightTransition();
+                        avatarsList.Add(avatar);
                         break;
                 }
             }
