b84058cc9983b888a16f9a919c944d6a5038a1cf
John Blaszczak <blaszczak.2@buckeyemail.osu.edu>
2021-10-26 17:14:57 -0400

    Set propery boundaries for mario based on level size.

--- Objects/MarioObjects/Mario.cs
+++ Objects/MarioObjects/Mario.cs
@@ -263,10 +263,9 @@ namespace GameSpace.GameObjects.BlockObjects
         private bool IsGoingToBeOutOfBounds(Vector2 newLocation)
         {
             if (Position.X + newLocation.X <= 0) { marioActionState.StandingTransition(); return true; }
-            if (Position.X + (CollisionBox.Width) + newLocation.X > 1400) { marioActionState.StandingTransition(); return true; }//should be max X value of level
-            //if (Position.X + (CollisionBox.Width) + newLocation.X > 12500) return true;//should be max X value of level
+            if (Position.X + (CollisionBox.Width) + newLocation.X > ((Rectangle)EntityManager.Camera.Limits).Width) { marioActionState.StandingTransition(); return true; }//should be max X value of level
             if (Position.Y + newLocation.Y <= 0) return true;
-            if (Position.Y + newLocation.Y >= 600) return true;
+            if (Position.Y + newLocation.Y >= ((Rectangle)EntityManager.Camera.Limits).Height) return true;
             return false;
         }
 
