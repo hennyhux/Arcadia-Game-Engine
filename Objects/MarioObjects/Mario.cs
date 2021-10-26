9bfa70e807671ab951d3af819c129ecd135d6c60
John Blaszczak <blaszczak.2@buckeyemail.osu.edu>
2021-10-26 16:59:00 -0400

    Mario stops running when reaching X boundary.

--- Objects/MarioObjects/Mario.cs
+++ Objects/MarioObjects/Mario.cs
@@ -91,7 +91,15 @@ namespace GameSpace.GameObjects.BlockObjects
         public void Update(GameTime gametime)
         {
             Vector2 newLocation = Velocity * (float)gametime.ElapsedGameTime.TotalSeconds;
-            if (!IsGoingToBeOutOfBounds(newLocation)) Position += newLocation;
+            if (!IsGoingToBeOutOfBounds(newLocation))
+            {
+                
+                Position += newLocation;
+            }
+            else
+            {
+
+            }
             //this.CollisionBox = new Rectangle((int)Position.X, (int)Position.Y, CollisionBox.Width, CollisionBox.Height);
             if(!(marioPowerUpState is GameSpace.States.BlockStates.DeadMarioState)){
                 if (marioPowerUpState is SmallMarioState)
@@ -254,10 +262,11 @@ namespace GameSpace.GameObjects.BlockObjects
 
         private bool IsGoingToBeOutOfBounds(Vector2 newLocation)
         {
-            if (Position.X + newLocation.X <= 0) return true;
-            if (Position.X + (CollisionBox.Width) + newLocation.X > 12500) return true;//should be max X value of level
+            if (Position.X + newLocation.X <= 0) { marioActionState.StandingTransition(); return true; }
+            if (Position.X + (CollisionBox.Width) + newLocation.X > 1400) { marioActionState.StandingTransition(); return true; }//should be max X value of level
+            //if (Position.X + (CollisionBox.Width) + newLocation.X > 12500) return true;//should be max X value of level
             if (Position.Y + newLocation.Y <= 0) return true;
-            if (Position.Y + newLocation.Y >= 450) return true;
+            if (Position.Y + newLocation.Y >= 600) return true;
             return false;
         }
 
