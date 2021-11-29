6998575a58014f8a777bffbd6c4ccfb6e6cb579e
CalebHernandez <caleb@buckeyefans.org>
2021-11-29 15:25:06 -0500

    CheatCodes

---
+++ Commands/SpawnFireFlowerCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GameSpace.Factories;
+using GameSpace.EntityManaging;
+using GameSpace.GameObjects.ItemObjects;
+using GameSpace.GameObjects.BlockObjects;
+using Microsoft.Xna.Framework;
+
+namespace GameSpace.Commands
+{
+    public class SpawnFireFlowerCommand : ICommand
+    {
+
+        private protected GameRoot game;
+        private Mario mario;
+
+
+        public SpawnFireFlowerCommand(GameRoot game)
+        {
+            this.game = game;
+        }
+
+        public void Execute()
+        {
+            mario = FinderHandler.GetInstance().FindMario();
+            Vector2 loc = new Vector2(mario.Position.X + 4, mario.Position.Y - 65);
+            FireFlower flower = (FireFlower)ObjectFactory.GetInstance().CreateFireFlowerObject(loc);//some location
+            TheaterHandler.GetInstance().AddItemToStage(flower);
+
+        }
+
+        public void Unexecute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Commands/SpawnKoopaCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using GameSpace.Factories;
+using GameSpace.EntityManaging;
+using GameSpace.GameObjects.EnemyObjects;
+using GameSpace.GameObjects.BlockObjects;
+using Microsoft.Xna.Framework;
+
+namespace GameSpace.Commands
+{
+    public class SpawnKoopaCommand : ICommand
+    {
+
+        private protected GameRoot game;
+        private Mario mario;
+
+
+        public SpawnKoopaCommand(GameRoot game)
+        {
+            this.game = game;
+        }
+
+        public void Execute()
+        {
+            Vector2 loc;
+            mario = FinderHandler.GetInstance().FindMario();
+            if (mario.Facing == Enums.MarioDirection.RIGHT)
+            {
+                loc = new Vector2(mario.Position.X + 75, mario.Position.Y - 15);
+            }
+            else
+            {
+                loc = new Vector2(mario.Position.X - 75, mario.Position.Y - 15);
+            }
+            GreenKoopa koopa = (GreenKoopa)ObjectFactory.GetInstance().CreateGreenKoopaObject(loc);
+            TheaterHandler.GetInstance().AddItemToStage(koopa);
+        }
+
+        public void Unexecute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
