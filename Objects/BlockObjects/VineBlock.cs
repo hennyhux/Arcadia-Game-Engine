e66096d6334682e146299bff2d7e06de95989187
Beto Cruz <cruzraygada.2@osu.edu>
2021-11-30 23:48:33 -0500

    code review

---
+++ Objects/BlockObjects/VineBlock.cs
@@ -0,0 +1,25 @@
+﻿using GameSpace.Abstracts;
+using GameSpace.Enums;
+using GameSpace.Factories;
+using GameSpace.States.BlockStates;
+using Microsoft.Xna.Framework;
+
+namespace GameSpace.GameObjects.BlockObjects
+{
+    public class VineBlock : AbstractBlock
+    {
+        public VineBlock(Vector2 initalPosition)
+        {
+            ObjectID = (int)BlockID.VINEBLOCK;
+            Position = initalPosition;
+            Sprite = SpriteBlockFactory.GetInstance().ReturnVineBlock();
+            CollisionBox = new Rectangle((int)Position.X, (int)Position.Y, Sprite.Texture.Width * 2, Sprite.Texture.Height * 2);
+            state = new StateVineBlock();
+        }
+
+        public override bool RevealItem()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
